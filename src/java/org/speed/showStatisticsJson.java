a72f7f0cd95d488a38778ad7f88f8311eb666627
yu qiancheng <yu_qiancheng@hotmail.com>
2020-04-11 17:01:02 +0800

    支持JSON

---
+++ src/java/org/speed/showStatisticsJson.java
@@ -0,0 +1,59 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.speed;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import net.sf.json.JSONArray;
+import org.speed.db.JDBCUtil;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
+
+/**
+ *
+ * @author yu_qi
+ */
+public class showStatisticsJson implements Controller {
+
+    private final String sql = "SELECT speed2,count(*) FROM speed group by speed2 order by speed2";
+
+    public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
+
+        ModelAndView mav = new ModelAndView("showStatisticsJson");
+
+        List<timeResult> page_result = new ArrayList<>();
+        Connection connection
+                = JDBCUtil.beginTransaction();
+        try {
+            PreparedStatement pst = connection.prepareStatement(sql);
+            ResultSet resultSet = pst.executeQuery();
+            System.out.println(this.toString() + connection.toString());
+            while (resultSet.next()) {
+                timeResult r = new timeResult();
+                r.setMessage(((double) Integer.valueOf(resultSet.getInt(1)) / 10) + "-" + ((double) (resultSet.getInt(1) + 1) / 10) + "秒 共" + resultSet.getString(2) + "人");
+                page_result.add(r);
+                System.out.println(r.getMessage());
+            }
+        } catch (SQLException e) {
+            System.out.println("异常提醒：" + e);
+        }
+
+        JDBCUtil.rollbackTransaction();
+
+        JSONArray jsonArray = JSONArray.fromObject(page_result);
+        System.out.println(jsonArray.toString());
+        mav.addObject("message", jsonArray.toString());
+
+        return mav;
+    }
+}
